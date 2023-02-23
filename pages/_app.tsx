0bb7c187dda09346919fc79007c66e6f0eaf1c7e
Oluwafemi Kayode <mrlapinho@gmail.com>
2023-02-23 02:51:40 +0000

    Add top navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from "next/app";
 
 import { Inter } from "@next/font/google";
 
+import Nav from "@/components/Nav";
+
 import "@/styles/globals.css";
 
 const inter = Inter({
@@ -12,6 +14,7 @@ const inter = Inter({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`${inter.variable} font-sans`}>
+      <Nav />
       <Component {...pageProps} />
     </div>
   );
