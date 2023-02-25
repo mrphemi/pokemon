640c3bec02eee2f0f642d787924e5e4a5bdca6c9
Oluwafemi Kayode <mrlapinho@gmail.com>
2023-02-25 02:24:19 +0000

    Add animation keyframes to tailwing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,21 @@ module.exports = {
           "light-blue": "#58569C",
         },
       },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+      },
     },
   },
   plugins: [],
